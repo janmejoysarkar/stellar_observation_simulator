c0373996a4028446b016e0485e84f7629d0a6664
janmejoy <janmejoysarker@gmail.com>
2024-11-18 20:00:26 +0530

    PSF weighted star

--- src/simulated_obs.py
+++ src/simulated_obs.py
@@ -5,17 +5,18 @@ import os
 from astropy.io import fits
 import time
 
-def sim_obs(del_c, del_r, peak_intensity, h=320, w=320, BIAS=1500):
+def sim_obs(del_c, del_r, total_intensity, h=320, w=320, BIAS=1500):
 
     current_file_path = os.path.abspath(__file__)
     project_path = os.path.dirname(current_file_path)
-
+    psf_path= os.path.join(project_path, "../data/interim/psf.fits")
+    psf= fits.open(psf_path)[0].data
     #Make a blank canvas
     canvas = np.zeros((h, w))
+    # Make the star
+    star= psf*total_intensity/np.sum(psf)
     # Replace a portion of the canvas with the pixel data 
-    canvas[int(h/2), int (w/2)] = peak_intensity
-    # Introduce convolution
-
+    canvas[int(h/2+del_r):int(h/2+del_r+np.shape(star)[0]), int(w/2+del_c):int(w/2+del_c+np.shape(star)[1])]=star
     # Introducing random Poisson noise.
     canvas = np.random.poisson(canvas) 
     # Read noise added separately
@@ -38,6 +39,6 @@ if __name__=='__main__':
        'BB03': 1389*1
     }
 
-   image= sim_obs(10, 10, peak_intensity=pe['NB02'])
+   image= sim_obs(10, 10, total_intensity=pe['NB07'])
    plt.imshow(image, origin='lower')
    plt.show()
