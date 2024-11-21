95ce45a715b098bac117d3fde10a4c72a4a876c9
janmejoy <janmejoysarker@gmail.com>
2024-11-21 23:43:39 +0530

    simulated observation code improved

--- src/simulated_obs.py
+++ src/simulated_obs.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+2024-11-21 15:57:17
+@author: janmejoyarch
+@hostname: suitpoc1
+
+DESCRIPTION
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage.morphology import disk
@@ -31,15 +42,17 @@ def sim_obs(del_c, del_r, ftr_name, h=320, w=320, BIAS=1500):
     }
     current_file_path = os.path.abspath(__file__)
     project_path = os.path.dirname(current_file_path)
-    psf_path= os.path.join(project_path, "../data/interim/psf.fits")
+    psf_path= os.path.join(project_path, "../data/external/psf.fits")
     psf= fits.open(psf_path)[0].data
     #Make a blank canvas
     canvas = np.zeros((h, w))
     # Make the star
     star= psf*pe[ftr_name]/np.sum(psf)
+    # Star geometry
+    star_row, star_col= int(np.shape(star)[0]/2), int(np.shape(star)[1]/2)
     # Replace a portion of the canvas with the pixel data 
-    canvas[int(h/2+del_r):int(h/2+del_r+np.shape(star)[0]),
-           int(w/2+del_c):int(w/2+del_c+np.shape(star)[1])]=star
+    canvas[int(h/2-star_row+del_r):int(h/2-star_row+del_r+np.shape(star)[0]),
+           int(w/2-star_col+del_c):int(w/2-star_col+del_c+np.shape(star)[1])]=star
     # Introducing random Poisson noise.
     canvas = np.random.poisson(canvas) 
     # Read noise added separately (RMS is 10. So mean value is set as BIAS+5)
@@ -48,10 +61,11 @@ def sim_obs(del_c, del_r, ftr_name, h=320, w=320, BIAS=1500):
     return canvas
 
 if __name__=='__main__':
+    PLOT, SAVE=True, True
     ftr_ls= ['NB01','NB02','NB03','NB04','NB05','NB06','NB07','NB08','BB01','BB02','BB03']
     for ftr_name in ftr_ls:
         image= sim_obs(0, 0, ftr_name)
-        fits.writeto(f'../products/{ftr_name}.fits', image, overwrite=True)
+        if SAVE: fits.writeto(f'../products/{ftr_name}.fits', image, overwrite=True)
         print(ftr_name)
         plt.imshow(image, origin='lower')
         plt.title(ftr_name)
